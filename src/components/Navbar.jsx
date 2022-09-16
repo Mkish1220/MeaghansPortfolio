040dfa6556a780f48fc82efb0b0e0b0f85a61560
MKish420 <meaghanmpeters@hotmail.com>
2022-09-16 12:40:12 -0400

    added nav bar working for responsiviness added homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react'
-import Logo from '../assets/logo.png'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import React, {useState} from 'react';
+import Logo from '../assets/logo.png';
+import {FaBars, FaTimes} from 'react-icons/fa';
+
 
 const Navbar = () => {
-    const {nav, setNav}= useState(false)
-    const handleClick = () => {setNav(!nav)
+    const [nav, setNav]= useState(false);
+    const handleClick = () => setNav(!nav);
 
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
@@ -12,7 +13,7 @@ const Navbar = () => {
     <img src={Logo} alt='Logo' style={{width: '50px' }} />
    </div>
 
-   {/* menu */}
+
     <ul className='hidden md:flex'>
         <li>Home</li>
         <li>About</li>
@@ -21,12 +22,12 @@ const Navbar = () => {
         <li>Contact</li>
     </ul>
 
-   {/* hamburger */}
-   <div onclick={handleClick} className='md:hidden z-10'>
-    <FaBars />
+
+   <div onClick={handleClick} className='md:hidden z-10'>
+   {!nav ? <FaBars /> : <FaTimes />}
    </div>
-   {/* mobile menu */}
-   <div>
+
+
     <ul className={!nav ? 'hidden' : 'absolute top-0 lrft-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
         <li className='py-6 text-4xl'>Home</li>
         <li className='py-6 text-4xl'>About</li>
@@ -34,15 +35,15 @@ const Navbar = () => {
         <li className='py-6 text-4xl'>Work</li>
         <li className='py-6 text-4xl'>Contact</li>
     </ul>
-    </div>
   
 
-
-
-   {/* social icons */}
-    <div className='hidden lg:flex fixed flex-col top-[35%] left-0'></div>
+    <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+       <ul>
+        <li></li>
+        </ul> 
     </div>
-    )
-}
+    </div>
+    );
+};
 
 export default Navbar;
