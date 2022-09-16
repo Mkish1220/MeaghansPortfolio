2fdb1e7dac4d116843b7359429915497f07f9796
MKish420 <meaghanmpeters@hotmail.com>
2022-09-16 14:31:39 -0400

    final touches

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,8 +14,8 @@ const Home = () => {
           I'm a Full Stack Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
+          I’m a full-stack developer specializing in building and occasionally
+          designing exceptional digital experiences. Currently, I’m focused on
           building responsive full-stack web applications.
         </p>
         <div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -99,7 +99,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://www.linkedin.com/in/meaghan-kish-4785141b1/'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
@@ -107,7 +107,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://github.com/Mkish1220'
             >
               Github <FaGithub size={30} />
             </a>
@@ -115,7 +115,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='/contact'
             >
               Email <HiOutlineMail size={30} />
             </a>
