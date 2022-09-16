cb449c175465d1d1ae7882c91fe90f1a824aecbc
MKish420 <meaghanmpeters@hotmail.com>
2022-09-16 13:35:30 -0400

    added work history for projects need better fit and project demo link to code once better

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
-// import Work from "./components/Work";
+import Work from "./components/Work";
 
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <Home />
       <About />
       <Skills />
-      {/* <Work /> */}
+      <Work />
       {/* <Contact /> */}
     </div>
   );
